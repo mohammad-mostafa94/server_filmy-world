9d81e59210aa4a633da608169230e6b1f97609be
Mohammad Mostafa <mohammad-mostafa94>
2021-11-14 07:10:34 +0600

    admin updated

--- index.js
+++ index.js
@@ -124,10 +124,11 @@ async function run() {
         app.get('/persons/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email: email };
-            const user = await usersCollection.findOne(query);
+            const person = await personCollection.findOne(query);
             let isAdmin = false;
-            if (user?.role === "admin") {
+            if (person?.role === "admin") {
                 isAdmin = true;
+
             }
             res.json(isAdmin);
         });
